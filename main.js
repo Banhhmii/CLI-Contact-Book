20c7dcfca2e499978c3978df21e1b1e83790e7c2
Banh <tommyngo156@gmail.com>
2026-04-23 15:53:45 -0400

    feat: add new contacts to contact book

---
+++ main.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')();
+const fs = require('fs');
+
+const data = fs.readFileSync('contact.json');
+const contacts = JSON.parse(data);
+
+function displayContacts() {
+    console.log("Contact List:");
+    for (const name in contacts) {
+        console.log(`${name}: ${contacts[name].phone}, ${contacts[name].email}`);
+    }
+}
+
+function addContact() {
+    const name = prompt("Enter contact name: ");
+    const phone = prompt("Enter contact phone number: ");
+    const email = prompt("Enter contact email: ");
+
+    contacts[name] = { phone, email };
+    fs.writeFileSync("contact.json", JSON.stringify(contacts, null, 2));
+    console.log("Contact added successfully!");
+}
+
+
